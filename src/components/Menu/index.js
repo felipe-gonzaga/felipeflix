e2307d7fffc961504507b9395b7c4e1d12ed82ba
Felipe Gonzaga Alves da Silva <felipe.silva@sefaz.se.gov.br>
2020-07-29 00:52:43 -0300

    Rotas mapeadas

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png'
 import './Menu.css';
-import ButtonLink from '../ButtonLink';
+import Button from '../Button';
+
+//import ButtonLink from '../ButtonLink';
 
 function Menu () {
     return (
         <nav className="Menu">
-            <a href="/">
+            <a to="/">
             <img className="Logo" src={Logo} alt="FelipeFlix logo" />
             </a>
 
-            <ButtonLink className="ButtonLink" href="/" >
+            <Button as={Link} className="ButtonLink" to="/cadastro/video" >
                 Novo Vídeo
-            </ButtonLink>
+            </Button>
         </nav>
     );
 }
